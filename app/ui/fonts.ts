cfed054522cecb9a6ca4f3430255a5f4d1679d8e
joshp423 <205880399+joshp423@users.noreply.github.com>
2026-05-14 14:23:04 +1200

    Refactored to monorepo non next.js platform

--- app/ui/fonts.ts
+++
@@ -1,5 +0,0 @@
-import { Roboto } from "next/font/google";
-import { DM_Sans } from "next/font/google";
-
-export const roboto = Roboto();
-export const dm_sans = DM_Sans({weight: "500"})
---
+++ apps/api/app.ts
@@ -0,0 +1,27 @@
+import "dotenv/config";
+import express from "express";
+import indexRouter from "./routes/indexRouter.js";
+import path from "node:path";
+import cors from "cors";
+const app = express();
+
+// Adds headers: Access-Control-Allow-Origin: *
+app.use(cors());
+
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const assetsPath = path.join(__dirname, "public");
+
+app.use(express.static(assetsPath));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+app.use("/", indexRouter);
+
+const PORT = process.env.PORT || 3000;
+
+export default app

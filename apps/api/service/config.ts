02bfe45125ea4c0597a0a53072d42dbddd06d5c1
joshp423 <205880399+joshp423@users.noreply.github.com>
2026-05-15 20:01:46 +1200

    updated config to remove cloudinary from template

--- apps/api/service/config.ts
+++ apps/api/service/config.ts
@@ -2,9 +2,6 @@ import z from "zod";
 
 const configSchema = z.object({
   DATABASE_URL: z.string().min(1),
-  CLOUDINARY_API_SECRET: z.string().min(1),
-  CLOUDINARY_CLOUD_NAME: z.string().min(1),
-  CLOUDINARY_API_KEY: z.string().min(1),
   JWT_SECRET: z.string().min(1),
 });
 

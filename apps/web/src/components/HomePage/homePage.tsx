039fd336df9d5982e83ff109de50dcbdb83e1291
joshp423 <205880399+joshp423@users.noreply.github.com>
2026-05-15 01:44:58 +0000

    updated HP and route files

---
+++ apps/web/src/components/HomePage/homePage.tsx
@@ -0,0 +1,13 @@
+
+
+export default function Homepage() {
+    return(
+        <div className="HpMain">
+            <h1>Easy Buckets</h1>
+            <h1>Looking for an <i>easy</i> way to score your basketball teams' games and view stats and other insights?</h1>
+            <p> Youre in the right place. </p>
+            <button>Sign Up</button>
+            <button>Log In</button>
+        </div>
+    )
+}
---
+++ apps/web/src/routes.tsx
@@ -0,0 +1,12 @@
+import App from "./App";
+import Homepage from "./components/Homepage/homepage";
+
+const routes = [
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <Homepage /> },
+    ],
+  },
+];

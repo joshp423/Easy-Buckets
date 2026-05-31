1381ac6b8c1246734e7cb712fbeedaf2d8324c14
joshp423 <205880399+joshp423@users.noreply.github.com>
2026-05-31 22:57:01 +1200

    updated css and some classes on loggedOutHP

--- apps/web/src/components/Homepage/loggedOutHP/loggedOutHP.tsx
+++ apps/web/src/components/Homepage/loggedOutHP/loggedOutHP.tsx
@@ -1,16 +1,21 @@
 import { Link } from "react-router";
+import "./loggedOutHP.css"
 
 export default function LoggedOutHP() {
   return (
     <div className="HpMain">
-      <h1>Easy Buckets</h1>
-      <h1>
+      <div className="hpTitle"><h1>Easy Buckets</h1></div>
+      <div className="hpBlurb">
+        <h1>
         Looking for an <i>easy</i> way to score your basketball teams' games and
         view stats and other insights?
-      </h1>
-      <p> Youre in the right place. </p>
-      <Link to={"/sign-up"}>Sign Up</Link>
-      <Link to={"/log-in"}>Log In</Link>
+        </h1>
+        <p> Youre in the right place. </p>
+        <div>
+          <Link to={"/sign-up"}>Sign Up</Link>
+        <Link to={"/log-in"}>Log In</Link>
+        </div>
+      </div>
     </div>
   );
 }

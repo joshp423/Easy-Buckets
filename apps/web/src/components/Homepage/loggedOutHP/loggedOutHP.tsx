5a655d2d786891f0c3763e07d9e093650770fbc8
joshp423 <205880399+joshp423@users.noreply.github.com>
2026-06-02 12:30:36 +1200

    loggedOutHP css and structure updates

--- apps/web/src/components/Homepage/loggedOutHP/loggedOutHP.tsx
+++ apps/web/src/components/Homepage/loggedOutHP/loggedOutHP.tsx
@@ -5,16 +5,20 @@ export default function LoggedOutHP() {
   return (
     <div className="HpMain">
       <div className="hpTitle"><h1>Easy Buckets</h1></div>
-      <div className="hpBlurb">
-        <h1>
-        Looking for an <i>easy</i> way to score your basketball teams' games and
-        view stats and other insights?
-        </h1>
-        <p> Youre in the right place. </p>
-        <div>
-          <Link to={"/sign-up"}>Sign Up</Link>
-        <Link to={"/log-in"}>Log In</Link>
+      <div>
+        <div className="hpBlurb">
+          <h1>
+          Looking for an <i>easy</i> way to score your basketball teams' games and
+          view stats and other insights?
+          </h1>
+          <p> Youre in the right place. </p>
+          <div>
+            <Link to={"/sign-up"}>Sign Up</Link>
+          <Link to={"/log-in"}>Log In</Link>
+          </div>
         </div>
+       
+        <div className="hpImage"></div>
       </div>
     </div>
   );

c7abb05593f1ef3d5a5576e8f3e2d84fa9533d5a
joshp423 <205880399+joshp423@users.noreply.github.com>
2026-06-11 11:42:34 +1200

    adjusted logic for +- buttons like pagination

--- apps/web/src/components/GameScoring/CreatePlayers/createPlayers.tsx
+++ apps/web/src/components/GameScoring/CreatePlayers/createPlayers.tsx
@@ -4,6 +4,9 @@ export default function CreatePlayers() {
 
     const [addPlayersAmount, setAddPlayersAmount] = useState<number>(1)
 
+    const canDecreasePlayers = () => (addPlayersAmount === 1 ? false : true);
+    const canIncreasePlayers = () => (addPlayersAmount >= 7 ? false : true);
+
     return(
         <div className="addPlayers">
             <h1>Add New Players</h1>
@@ -14,8 +17,8 @@ export default function CreatePlayers() {
                         <input type="number" placeholder="Player Number"/>
                     </div>
                 ))}
-                <button type="button" onClick={() => {setAddPlayersAmount(addPlayersAmount -1)}}>-</button>
-                <button type="button" onClick={() => {setAddPlayersAmount(addPlayersAmount +1)}}>+</button>
+                <button type="button" onClick={() => canDecreasePlayers() && setAddPlayersAmount(addPlayersAmount - 1)}>-</button>
+                <button type="button" onClick={() => canIncreasePlayers() && setAddPlayersAmount(addPlayersAmount + 1)}>+</button>
                 <button type="submit">Add Players</button>
             </form>
         </div>

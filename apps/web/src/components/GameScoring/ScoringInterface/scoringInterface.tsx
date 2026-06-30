eccba308e1fbe081e6ced143ac983f49467863f4
joshp423 <205880399+joshp423@users.noreply.github.com>
2026-06-30 23:06:51 +1200

    added videoPlayer if replay + court zoom

--- apps/web/src/components/GameScoring/ScoringInterface/scoringInterface.tsx
+++ apps/web/src/components/GameScoring/ScoringInterface/scoringInterface.tsx
@@ -5,6 +5,7 @@ import StatSelection from "./StatSelection/statSelection";
 import CourtInterface from "./CourtInterface/courtInterface";
 import "./scoringInterface.css";
 import type { Game } from "../../../types/game";
+import VideoPlayer from "../../Homepage/Dashboard/GameDisplay/GameStats/VideoPlayer/videoPlayer";
 
 type ScoringInterfaceProps = {
   selectedPlayers: Player[];
@@ -21,8 +22,38 @@ export default function ScoringInterface({
   const [selectedPlayer, setSelectedPlayer] = useState<number | null>(null);
   const [selectedStat, setSelectedStat] = useState<string>("");
 
+  if (!gameDetails.replay) return (
+    <div className="scoringInterface">
+      <div className="interfaceInput">
+        <PlayerSelection
+          selectedPlayers={selectedPlayers}
+          setSelectedPlayer={setSelectedPlayer}
+          selectedPlayer={selectedPlayer}
+          selectedUI={selectedUI}
+          setSelectedUI={setSelectedUI}
+        />
+        <StatSelection
+          setSelectedStat={setSelectedStat}
+          selectedStat={selectedStat}
+          selectedPlayer={selectedPlayer}
+          setSelectedPlayer={setSelectedPlayer}
+          selectedUI={selectedUI}
+          setSelectedUI={setSelectedUI}
+        />
+        <CourtInterface
+          selectedStat={selectedStat}
+          selectedPlayer={selectedPlayer}
+          setSelectedPlayer={setSelectedPlayer}
+          selectedUI={selectedUI}
+          setSelectedUI={setSelectedUI}
+        />
+      </div>
+    </div>
+  )
+
   return (
     <div className="scoringInterface">
+      <VideoPlayer videoUrl={gameDetails.replay}/>
       <div className="interfaceInput">
         <PlayerSelection
           selectedPlayers={selectedPlayers}

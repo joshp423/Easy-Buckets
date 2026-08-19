a8432f23000a716cdf821ca8006f99a1ae1d78bf
joshp423 <205880399+joshp423@users.noreply.github.com>
2026-08-19 17:27:28 +1200

    added apiReq files wip, added onchange to form

---
+++ apps/web/src/components/TeamSeasons/PlayerListEdit/EditPlayer/editPlayerDetailsAPIReq.ts
@@ -0,0 +1,9 @@
+import type { Player } from "../../../../types/player"
+
+type editPlayerDetailsAPIReqArgs = {
+    player: Player
+}
+
+export default function editPlayerDetailsAPIReq() {
+
+}
--- apps/web/src/components/TeamSeasons/PlayerListEdit/EditPlayer/editplayer.tsx
+++ apps/web/src/components/TeamSeasons/PlayerListEdit/EditPlayer/editplayer.tsx
@@ -1,14 +1,30 @@
 import type { Player } from "../../../../types/player"
+import { useState, type SyntheticEvent } from "react";
 
 type EditPlayerProps = {
     editPlayer: Player;
 }
 
 export default function EditPlayer({editPlayer}: EditPlayerProps) {
-    return(
+    const [playerName, setPlayerName] = useState<string>(editPlayer.name);
+    const [playerNumber, setPlayerNumber] = useState<number>(editPlayer.number);
+
+    function confirmPlayerEdit(e: SyntheticEvent<HTMLFormElement>) {
+        e.preventDefault();
+        
+    }
+
+    function deletePlayer(){}
+    
+    return (
         <div className="editPlayer">
-            <form action="">
-                
+            <form onSubmit={confirmPlayerEdit}>
+                <label htmlFor="playerName">Edit Player Name: </label>
+                <input onChange={(e) => {setPlayerName(e.target.value)}} type="text" name="playerName" value={editPlayer.name}/>
+                <label htmlFor="playerName">Edit Player Number: </label>
+                <input onChange={(e) => {setPlayerNumber(Number(e.target.value))}} type="number" name="playerName" value={editPlayer.number}/>
+                <button type="button">Delete Player from team</button>
+                <button type="submit">Confirm Changes</button>
             </form>
         </div>
     )

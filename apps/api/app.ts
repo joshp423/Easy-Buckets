062ae81aedbec8e430a6f785a93bdb4a8cf47d80
joshp423 <205880399+joshp423@users.noreply.github.com>
2026-09-01 10:35:10 +1200

    adjusted api setup for  vercel

--- apps/api/app.ts
+++ apps/api/app.ts
@@ -1,34 +1,20 @@
 import "dotenv/config";
 import express from "express";
 import indexRouter from "./routes/indexRouter.js";
-import path from "node:path";
 import cors from "cors";
 const app = express();
 
-// Adds headers: Access-Control-Allow-Origin: *
 app.use(cors());
-
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const assetsPath = path.join(__dirname, "public");
-
-app.use(express.static(assetsPath));
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.json());
 
 app.use("/", indexRouter);
 
-const PORT = process.env.PORT || 3000;
+if (process.env.VERCEL !== "1") {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Testing-backend - listening on port ${PORT}!`);
+  });
+}
 
-// export default app
-
-app.listen(PORT, (error) => {
-  if (error) {
-    throw error;
-  }
-  console.log(`Testing-backend - listening on port ${PORT}!`);
-});
+export default app;

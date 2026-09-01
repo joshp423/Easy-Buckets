3db4b297980b64a6272a69dbb0d506924d89197f
joshp423 <205880399+joshp423@users.noreply.github.com>
2026-09-01 17:30:38 +1200

    fixed formatting alignment problem

--- apps/web/src/components/SignUp/signUp.tsx
+++ apps/web/src/components/SignUp/signUp.tsx
@@ -16,57 +16,59 @@ export default function SignUp() {
   const [success, setSuccess] = useState<boolean>(false);
 
   return (
-    <div className="signUp">
-      <div className="signUpTitle">
-        <FontAwesomeIcon icon={faBasketball} />
-        <h1
-          onClick={() => {
-            navigate("/");
-          }}
-        >
-          Easy Buckets
-        </h1>
-      </div>
-      <form
-        onSubmit={(e) =>
-          signUpAPI({ e, setLoading, setErrors, email, password, navigate, setSuccess })
-        }
-      >
-        <div className="signUpForm">
-          <div className="errorHandling">
-            {errors?.map((error) => (
-              <li key={error}>{error}</li>
-            ))}
-          </div>
-          <h1>Sign Up</h1>
-          <label htmlFor="email">Email: </label>
-          <div className="signUpFormInput">
-            <FontAwesomeIcon icon={faEnvelope} />
-            <input
-              name="email"
-              id="email"
-              type="text"
-              required
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-
-          <label htmlFor="password"> Password: </label>
-          <div className="signUpFormInput">
-            <FontAwesomeIcon icon={faKey} />
-            <input
-              name="password"
-              id="password"
-              type="password"
-              required
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <button type="submit">
-            <div style={success ? {"backgroundColor": "green"} : {}}>{loading ? <LoadingBall /> : <p>Submit</p>}</div>
-          </button>
+    <div className="signUpContainer">
+      <div className="signUp">
+        <div className="signUpTitle">
+          <FontAwesomeIcon icon={faBasketball} />
+          <h1
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            Easy Buckets
+          </h1>
         </div>
-      </form>
+        <form
+          onSubmit={(e) =>
+            signUpAPI({ e, setLoading, setErrors, email, password, navigate, setSuccess })
+          }
+        >
+          <div className="signUpForm">
+            <div className="errorHandling">
+              {errors?.map((error) => (
+                <li key={error}>{error}</li>
+              ))}
+            </div>
+            <h1>Sign Up</h1>
+            <label htmlFor="email">Email: </label>
+            <div className="signUpFormInput">
+              <FontAwesomeIcon icon={faEnvelope} />
+              <input
+                name="email"
+                id="email"
+                type="text"
+                required
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+
+            <label htmlFor="password"> Password: </label>
+            <div className="signUpFormInput">
+              <FontAwesomeIcon icon={faKey} />
+              <input
+                name="password"
+                id="password"
+                type="password"
+                required
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <button type="submit">
+              <div style={success ? {"backgroundColor": "green"} : {}}>{loading ? <LoadingBall /> : <p>Submit</p>}</div>
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
